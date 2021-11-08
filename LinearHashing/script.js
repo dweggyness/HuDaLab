dcc528aadbad5147212aa5dfadcfa17cfd476961
MstXy <61139466+MstXy@users.noreply.github.com>
2021-11-08 22:18:48 +0800

    Update functions of LHT; basics for visualization

--- LinearHashing/script.js
+++ LinearHashing/script.js
@@ -167,6 +167,37 @@ class LinearHashingTable {
     if (DEBUG_FLAG) this.display();
   }
 
+  /* takes a new EHT as param, deepcopy from original EHT to a new EHT object.
+  */
+  deepcopyto(tempLHT) {
+    let buckets = [];
+    for (var i = 0; i < this.buckets.length; i++) {
+      let bucket = this.buckets[i];
+      let newBucket = new Bucket();
+      newBucket.data = JSON.parse(JSON.stringify(bucket.data));
+      newBucket.hasOverFlowPage = bucket.hasOverFlowPage;
+      if (bucket.hasOverFlowPage) {
+        newBucket.overflowPage = new Bucket();
+        newBucket.overflowPage.hasOverFlowPage = bucket.overflowPage.hasOverFlowPage;
+        newBucket.overflowPage.data = JSON.parse(JSON.stringify(bucket.overflowPage.data));
+      }
+      buckets[i] = newBucket;
+    }
+    tempLHT.round = this.round;
+    tempLHT.splitPointer = this.splitPointer;
+    tempLHT.numBuckets = this.numBuckets;
+    tempLHT.buckets = buckets;
+  }
+
+  /* Cleans LHT, reset this.buckets, i.e. reinitialize LHT
+  */
+  clean() {
+    this.round = 0;
+    this.splitPointer = 0;
+    this.numBuckets = 4;
+    this.buckets = [new Bucket(), new Bucket(), new Bucket(), new Bucket()];
+  }
+
   // helper function to display and debug and look around
   display() {
     console.log("Round: ", this.round, " Splitpointer: ", this.splitPointer, " Bucket size:", bucketSize, "\n");
@@ -181,6 +212,7 @@ class LinearHashingTable {
   }
 }
 
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 // input: buckets is an array of Bucket instances
 function flatOutBuckets(buckets) {
   let flatted = [];
@@ -211,7 +243,7 @@ function flatOutBuckets(buckets) {
   return flatted
 }
 
-
+// main Viz helper functions
 function getKeyLocation(d, i) {
   let y = 100 + i * 25;
   return "translate(" + 10 + ", " + y + ")"
@@ -226,15 +258,40 @@ function getBucketLocation(d) {
   if (d["overflow"]) {
     x += 150;
   }
-  x += 200 + d["order"] * 30;
+  x += 50 + d["order"] * 30;
   y += 100 + d["bucket"] * 25;
   return "translate(" + x + ", " + y + ")"
-
+}
+function getSplitLocation(d) {
+  return "translate(" + -10 + ", " + d*25 + ")"
+}
+function getPointerLocation(d) {
+  return "translate(" + -10 + ", " + d*25 + ")"
+}
+// processlog helper functions =========================================
+function findLogBtnY(idx) {
+  idx = idx - 1;
+  return idx*28
 }
 
+// bbox helper functions ===============================================
+function findHashBBox(idx) {
+  return d3.select("#hashBtn" + idx).node().getBBox();
+}
+function findInsertBBox(idx) {
+  return d3.select("#insertBtn" + idx).node().getBBox();
+}
+function findSplit1BBox(idx) {
+  return d3.select("#split1Btn" + idx).node().getBBox();
+}
+function findSplit2BBox(idx) {
+  return d3.select("#split2Btn" + idx).node().getBBox();
+}
+function findPointerBBox() {
+  return d3.select("#splitPointer").node().getBBox();
+}
 
-
-
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 // initialize canvas
 var width = 900;
 var height = 600;
@@ -274,36 +331,57 @@ var svg = d3.select("#container-1")
 let vizSection = svg
     .append("g")
       .attr("id", "viz-container")
-      .attr("transform", "translate(350, 0)");
+      .attr("transform", "translate(400, 0)");
+// add container for process log
+let processLog = svg
+    .append("g")
+      .attr("id", "process-container")
+      .attr("transform", "translate(0, 70)");
 
 let keyPart = vizSection.append("g").attr("class", "keyPart");
 // let arrowPart = vizSection.append("g").attr("class", "arrowPart");
 let bucketPart = vizSection.append("g").attr("class", "bucketPart");
+let pointerPart = vizSection.append("g").attr("class", "pointerPart")
+      // hide split pointer at first
+      .attr("opacity", "0");
+
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// splitPointer
+pointerPart
+    .append("text")
+      .attr("id", "splitPointer")
+      .text("splitPointer")
+      .attr("text-anchor", "middle")
+      .attr("x", -60)
+      .attr("y", 100)
+      .attr("fill", "#000000");
+pointerPart
+    .insert("rect", "text")
+      .attr("id", "splitPointerBBox")
+      .attr("class", "bBox")
+      .attr("x", function () {
+        return findPointerBBox().x - btnPaddingX;
+      })
+      .attr("y", function () {
+        return findPointerBBox().y - btnPaddingY;
+      })
+      .attr("width", function () {
+        return findPointerBBox().width + 2*btnPaddingX;
+      })
+      .attr("height", function () {
+        return findPointerBBox().height + 2*btnPaddingY;
+      })
+      .attr("stroke", "#3f66bc")
+      .attr("fill", "#e0e9fa");
 
 
-const LHT = new LinearHashingTable();
-
-const NUM_ELEMS_TO_INSERT = 40;
-// insert (10 to NUM_ELEMS_TO_INSERT + 10) into the table in a random order
-let arrayOfNums = [];
-const startingNum = 10;
-for (var i = startingNum; i < NUM_ELEMS_TO_INSERT + startingNum; i++) {
-  arrayOfNums[i - startingNum] = i + 1;
-}
-for (var i = startingNum; i< NUM_ELEMS_TO_INSERT + startingNum; i++ ) {
-  const val = arrayOfNums[Math.floor(Math.random() * arrayOfNums.length)];
-  arrayOfNums = arrayOfNums.filter(function(elem) { return elem !== val })
-
-  LHT.insertIntoTable(val);
-}
-
-
-
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 function drawViz(lht) {
 
   console.log(lht);
   let bucketData = lht.buckets;
 
+  //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
   // keys
   let keyGroup = keyPart
       .selectAll(".keyGroup")
@@ -329,6 +407,16 @@ function drawViz(lht) {
         .attr("y", 0)
         .attr("fill", "#000000");
 
+  keys
+      .insert('rect','text')
+        .attr("class", "keyBBox bBox")
+        .attr("x", -15)
+        .attr("y", -15.5)
+        .attr("width", 30)
+        .attr("height", 20)
+        .attr("stroke", "rebeccapurple")
+        .attr("fill", "#f1ebff");
+
   keys.attr("transform", getKeyIniLocation).transition().attr("transform", getKeyLocation);
 
   let exitingKeys = keyGroup.exit();
@@ -337,6 +425,7 @@ function drawViz(lht) {
   // update
   keyGroup.transition().duration(500).attr("transform", getKeyLocation);
 
+  //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
   // buckets & values
   let flattedData = flatOutBuckets(bucketData);
   console.log(flattedData);
@@ -362,6 +451,16 @@ function drawViz(lht) {
         .attr("y", 0)
         .attr("fill", "#000000");
 
+  values.insert("rect", "text")
+    .attr("id", d => "valueBBox" + d["key"])
+    .attr("class", "valueBBox bBox")
+    .attr("x", -15)
+    .attr("y", -15.5)
+    .attr("width", 30)
+    .attr("height", 20)
+    .attr("stroke", "#3f66bc")
+    .attr("fill", "#e0e9fa");
+
   values.transition().attr("transform", getBucketLocation);
 
   //exitingValues
@@ -371,7 +470,320 @@ function drawViz(lht) {
   // update
   valueGroup.transition().attr("transform", getBucketLocation);
 
+  //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+  // update splitPointer
+  pointerPart.attr("opacity", "100");
+  pointerPart.transition().attr("transform", function () {
+    return getPointerLocation(lht.splitPointer)
+  });
+
 }
 
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+function showNum(idx) {
+  processLog
+    .append("text")
+      .attr("class", "textBtn clickedText")
+      .attr("id", function () {
+        return "hashed"+idx;
+      })
+      .text(insertLog[idx])
+      .attr("text-anchor", "middle")
+      .attr("x", 225)
+      .attr("y", function() {
+        return findLogBtnY(idx)
+      })
+      .on("click", function () {
 
-drawViz(LHT);
+      });
+
+  processLog
+    .insert("rect", "text")
+      .attr("class", "bBox clickedBBox")
+      .attr("id", function () {
+        return "hashedBBox"+idx
+      })
+      .attr("x", 210)
+      .attr("y", function () {
+        return findLogBtnY(idx)-15.5;
+      })
+      .attr("width", 30)
+      .attr("height", 20);
+}
+
+function showHash(idx) {
+  processLog
+    .append("text")
+      .text("hash")
+      .attr("x", 0)
+      .attr("y", function () {
+        return findLogBtnY(idx)
+      })
+      .attr("class", "textBtn clickedText")
+      .attr("id", function () {
+        return "hashBtn" + idx
+      })
+      .on("click", function () {
+        hashClicked(idx);
+      });
+
+  processLog
+    .insert("rect", "text")
+      .attr("class", "bBox clickedBBox")
+      .attr("id", function () {
+        return "hashBtnBBox"+idx
+      })
+      .attr("x", function () {
+        return findHashBBox(idx).x - btnPaddingX;
+      })
+      .attr("y", function () {
+        return findHashBBox(idx).y - btnPaddingY;
+      })
+      .attr("width", function () {
+        return findHashBBox(idx).width + 2*btnPaddingX;
+      })
+      .attr("height", function () {
+        return findHashBBox(idx).height + 2*btnPaddingY;
+      });
+}
+
+function hashClicked(idx) {
+
+}
+
+function showInsert(idx) {
+  processLog
+    .append("text")
+      .text("insert")
+      .attr("x", 46)
+      .attr("y", function () {
+        return findLogBtnY(idx)
+      })
+      .attr("class", "textBtn clickedText")
+      .attr("id", function () {
+        return "insertBtn" + idx
+      })
+      .on("click", function () {
+        insertClicked(idx);
+      });
+
+  processLog
+    .insert("rect", "text")
+      .attr("class", "bBox clickedBBox")
+      .attr("id", function () {
+        return "insertBtnBBox"+idx
+      })
+      .attr("x", function () {
+        return findInsertBBox(idx).x - btnPaddingX;
+      })
+      .attr("y", function () {
+        return findInsertBBox(idx).y - btnPaddingY;
+      })
+      .attr("width", function () {
+        return findInsertBBox(idx).width + 2*btnPaddingX;
+      })
+      .attr("height", function () {
+        return findInsertBBox(idx).height + 2*btnPaddingY;
+      });
+}
+function insertClicked(idx) {
+
+}
+
+function showSplit1(idx) {
+  processLog
+    .append("text")
+      .text("split1")
+      .attr("x", 96)
+      .attr("y", function () {
+        return findLogBtnY(idx)
+      })
+      .attr("class", "textBtn clickedText")
+      .attr("id", function () {
+        return "split1Btn" + idx
+      })
+      .on("click", function () {
+        split1Clicked(idx);
+      });
+
+  processLog
+    .insert("rect", "text")
+      .attr("class", "bBox clickedBBox")
+      .attr("id", function () {
+        return "split1BtnBBox"+idx
+      })
+      .attr("x", function () {
+        return findSplit1BBox(idx).x - btnPaddingX;
+      })
+      .attr("y", function () {
+        return findSplit1BBox(idx).y - btnPaddingY;
+      })
+      .attr("width", function () {
+        return findSplit1BBox(idx).width + 2*btnPaddingX;
+      })
+      .attr("height", function () {
+        return findSplit1BBox(idx).height + 2*btnPaddingY;
+      });
+}
+function split1Clicked(idx) {
+
+}
+
+function showSplit2(idx) {
+  processLog
+    .append("text")
+      .text("split2")
+      .attr("x", 145)
+      .attr("y", function () {
+        return findLogBtnY(idx);
+      })
+      .attr("class", "textBtn clickedText")
+      .attr("id", function () {
+        return "split2Btn" + idx
+      })
+      .on("click", function () {
+        split2Clicked(idx);
+      });
+
+  processLog
+    .insert("rect", "text")
+      .attr("class", "bBox clickedBBox")
+      .attr("id", function () {
+        return "split2BtnBBox"+idx
+      })
+      .attr("x", function () {
+        return findSplit2BBox(idx).x - btnPaddingX;
+      })
+      .attr("y", function () {
+        return findSplit2BBox(idx).y - btnPaddingY;
+      })
+      .attr("width", function () {
+        return findSplit2BBox(idx).width + 2*btnPaddingX;
+      })
+      .attr("height", function () {
+        return findSplit2BBox(idx).height + 2*btnPaddingY;
+      });
+}
+function split2Clicked(idx) {
+
+}
+
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// global variables and states
+const LHT = new LinearHashingTable();
+
+let initialLHT = new LinearHashingTable();
+LHT.deepcopyto(initialLHT);
+let lhtRecord = [initialLHT];
+
+var insertIdx = 0;
+let insertLog = [null];
+// let stateHash = [false];
+// let stateInsert = [false];
+// let stateSplit1 = [false];
+// let stateSplit2 = [false];
+
+var newIniNum;
+
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// User Initiate Numbers of Insertion
+// create a seperate div for each interactive part
+let userNumDiv = document.createElement("div");
+userNumDiv.setAttribute("class", "btndiv");
+userNumDiv.setAttribute("id", "divIniNum");
+document.getElementById("button-container").appendChild(userNumDiv);
+
+let userNum = document.createElement("INPUT");
+userNum.setAttribute("type", "number");
+userNum.setAttribute("placeholder", "Positive integer");
+userNum.setAttribute("id", "userNum");
+userNum.setAttribute("style", "width:150px");
+userNum.addEventListener("keypress", (e) => {
+  if (e.key == "Enter") {
+    e.preventDefault();
+    iniLHT();
+  }
+});
+let submitNumBtn = document.createElement("BUTTON");
+submitNumBtn.setAttribute("type", "button");
+submitNumBtn.setAttribute("onclick", "iniLHT()")
+submitNumBtn.innerHTML = "Submit";
+submitNumBtn.setAttribute("id", "submitNumBtn");
+var numText = document.createElement("p");
+numText.setAttribute("class", "btnText");
+numText.innerHTML = "# of keys to insert:";
+document.getElementById("divIniNum").appendChild(numText);
+document.getElementById("divIniNum").appendChild(userNum);
+document.getElementById("divIniNum").appendChild(submitNumBtn);
+
+
+function iniLHT() {
+  newIniNum = parseFloat(userNum.value);
+  userNum.value = '';
+  // check if num is valid
+  if (newIniNum < 0 || newIniNum % 1 !== 0){
+    alert("Please enter a valid number! \n A valid number is a Positve Integer.")
+    newIniNum = null;
+  } else {
+    // reset LHT
+    LHT.clean();
+    // reset global variables
+    lhtRecord = [initialLHT];
+    insertIdx = 0;
+    insertLog = [null];
+    // clear process log
+    d3.select("#process-container").selectAll("*").remove();
+    d3.selectAll(".finding").remove();
+    d3.selectAll(".findingStatic").remove();
+
+
+    const NUM_ELEMS_TO_INSERT = newIniNum;
+    // insert (10 to NUM_ELEMS_TO_INSERT + 10) into the table in a random order
+    let arrayOfNums = [];
+    const startingNum = 10;
+    for (var i = startingNum; i < NUM_ELEMS_TO_INSERT + startingNum; i++) {
+      arrayOfNums[i - startingNum] = i + 1;
+    }
+    for (var i = startingNum; i< NUM_ELEMS_TO_INSERT + startingNum; i++ ) {
+      const val = arrayOfNums[Math.floor(Math.random() * arrayOfNums.length)];
+      arrayOfNums = arrayOfNums.filter(function(elem) { return elem !== val })
+
+      //update states
+      insertIdx++;
+
+      // LHT.insertIntoTable(val);
+      // code equivalent to LHT.insertIntoTable(val):=======================
+      const key = LHT.getIndex(val);
+
+      insertLog.push(val);
+
+      const bucket = LHT.buckets[key];
+      if (DEBUG_FLAG) console.log(`Insert ${val} into bucket ${key}`);
+
+      if (bucket.isFull()) {
+        if (DEBUG_FLAG) console.log(`Bucket full`);
+        bucket.insert(val, val);
+        LHT.splitBucket();
+        // call viz function
+        showNum(insertIdx);
+        showHash(insertIdx);
+        showInsert(insertIdx);
+        showSplit1(insertIdx);
+        showSplit2(insertIdx);
+      } else {
+        bucket.insert(val, val);
+        // call viz function
+        showNum(insertIdx);
+        showHash(insertIdx);
+        showInsert(insertIdx);
+      }
+      if (DEBUG_FLAG) LHT.display();
+      // equivalent code ends =============================================
+
+      let tempLHT = new LinearHashingTable();
+      LHT.deepcopyto(tempLHT);
+      lhtRecord.push(tempLHT);
+    }
+    drawViz(LHT);
+  }
+}
